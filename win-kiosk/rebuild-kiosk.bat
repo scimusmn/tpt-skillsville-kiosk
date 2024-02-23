1c42058e76257cea2f6f5ef672ba1d46e13cdfb1
Trygve Nordberg <tnordberg@smm.org>
2024-02-23 12:25:01 -0600

    feat: placeholder for kiosk startup scripts

---
+++ win-kiosk/rebuild-kiosk.bat
@@ -0,0 +1,5 @@
+#T his should be placed in the parent directory of the tpt-skillsville-kiosk
+# Clients can double-click this file after making content edits to rebuild the application with new content
+
+cd ./tpt-skillsville-kiosk
+yarn rebuild-kiosk
